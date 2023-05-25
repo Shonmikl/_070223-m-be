febb43ca5acf68b3362fd5cc38734b2c9409113f
shonmikl <mikhailegorov676@gmail.com>
2023-05-25 11:31:40 +0200

    arr task

---
+++ src/main/java/org/example/_2023_05_25/CollEx1.java
@@ -0,0 +1,35 @@
+package org.example._2023_05_25;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class CollEx1 {
+    private boolean isExist(int n, int k) {
+        //
+        //заполнить ЛЛ или АЛ рандомными числами n
+        //есть ли в АЛ или ЛЛ k
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        LinkedList<String> strings = new LinkedList<>();
+
+
+
+
+//        for (int i = 0; i < 15; i++) {
+//            list.add(new Random().nextInt(22));
+//        }
+//        System.out.println(list);
+//
+//        System.out.println(list.isEmpty());
+
+
+    }
+}
+
+interface I {
+    void get();
+}
