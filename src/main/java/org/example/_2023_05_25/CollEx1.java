cc2e90dde35475f8d39ac1a5e355c75f9758f8f6
shonmikl <mikhailegorov676@gmail.com>
2023-05-25 13:03:03 +0200

    arr task

--- src/main/java/org/example/_2023_05_25/CollEx1.java
+++ src/main/java/org/example/_2023_05_25/CollEx1.java
@@ -1,23 +1,39 @@
 package org.example._2023_05_25;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.awt.desktop.QuitStrategy;
+import java.util.*;
 
 public class CollEx1 {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    private static void getSumPairs(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Random().nextInt(20));
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == k) {
+                    System.out.println(list.get(i) + " : " + list.get(j));
+                }
+            }
+        }
+    }
+
+
     private boolean isExist(int n, int k) {
         //
         //заполнить ЛЛ или АЛ рандомными числами n
         //есть ли в АЛ или ЛЛ k
         return false;
     }
+
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        LinkedList<String> strings = new LinkedList<>();
-
-
-
+        ArrayList<QuitStrategy> list = new ArrayList<>();
+        int result = sum(sum(5, 2), sum(9, sum(3, 8)));
 
 //        for (int i = 0; i < 15; i++) {
 //            list.add(new Random().nextInt(22));
