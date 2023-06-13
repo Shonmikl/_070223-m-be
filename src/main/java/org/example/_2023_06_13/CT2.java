80ac3a2d448a9f0fdb1f54ea1f3cbc469d8b4e2b
shonmikl <mikhailegorov676@gmail.com>
2023-06-13 10:46:25 +0200

    t2

---
+++ src/main/java/org/example/_2023_06_13/CT2.java
@@ -0,0 +1,25 @@
+package org.example._2023_06_13;
+
+import java.util.List;
+import java.util.Set;
+
+public class CT2 {
+
+    /**
+     * 1. Найти все ключи в Map<String, Integer>, соответствующие заданному значению, и вернуть их в виде списка.
+     * public static List<String> findKeys(Map<String, Integer> inputMap, int targetValue)
+     *
+     * 2. Проверить, содержит ли Map<String, Integer> хотя бы одну запись, у которой значение больше заданного порога.
+     * public static boolean hasValueAboveThreshold(Map<String, Integer> inputMap, int threshold)
+     *
+     * 3.Проверить, все ли значения в Map<Integer, Boolean> равны true.
+     * public static boolean allValuesTrue(Map<Integer, Boolean> inputMap)
+     *
+     * 4. Найти сумму всех значений(int) в Map<String, Integer>, которые соответствуют заданному ключевому набору.
+     * public static int calculateSum(Map<String, Integer> inputMap, String[] keys)
+     *
+     * 5.Получить все ключи Map<Integer, Boolean>, у которых значение равно true, и вернуть их в виде списка.
+     * public static List<Integer> findTrueKeys(Map<Integer, Boolean> inputMap)
+     */
+
+}
