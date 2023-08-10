4b87357fc11fbb2176174281298af72c867a2065
shonmikl <mikhailegorov676@gmail.com>
2023-08-10 09:46:46 +0200

    test1

---
+++ src/main/java/org/example/_2023_08_10/Ex1222.java
@@ -0,0 +1,20 @@
+package org.example._2023_08_10;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Ex1222 {
+
+    @BeforeEach
+    void setUp() {
+        // clear()
+        // add
+        // add
+        // add
+    }
+
+    @Test
+    void test() {
+
+    }
+}
