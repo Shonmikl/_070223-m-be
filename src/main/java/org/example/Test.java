f4d4482c89b88b1928c0cd54713766d8af6e46ed
shonmikl <mikhailegorov676@gmail.com>
2023-09-28 11:11:19 +0200

    proxy

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,22 +1,16 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class Test {
 
     private static void test(String... s) {
 
     }
     public static void main(String[] args) {
-//        test();
-//        List<Integer> list = new ArrayList<>();
-//        boolean s = list.add(2);
-
-        Set<Integer> set = new HashSet<>();
-        boolean a1 = set.add(4);
-        boolean a2 = set.add(41);
-        boolean a3 = set.add(41);
-        System.out.println(a1 + "||" + a2 + "||" + a3);
+        Test test = new Test();
     }
 }
---
+++ src/main/java/org/example/_2023_09_28/Car.java
@@ -0,0 +1,48 @@
+package org.example._2023_09_28;
+
+public interface Car {
+   void start();
+   void stop();
+}
+
+class RealCar implements Car {
+    @Override
+    public void start() {
+        System.out.println("Real Car start.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Real Car STOP!");
+    }
+}
+
+class ProxyCar implements Car {
+    private final Car realCar;
+
+    public ProxyCar(Car realCar) {
+        this.realCar = realCar;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Logging start.");
+        realCar.start();
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Logging stop.");
+        realCar.stop();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car realCar = new RealCar();
+        Car proxyCar = new ProxyCar(realCar);
+
+        proxyCar.start();
+        proxyCar.stop();
+    }
+}
