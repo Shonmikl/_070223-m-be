2512cc2f14561eb7f816f068c39f5d34f56fa397
shonmikl <mikhailegorov676@gmail.com>
2023-10-10 12:34:51 +0200

    logger

--- src/main/java/org/example/_2023_10_10/LogClass.java
+++ src/main/java/org/example/_2023_10_10/LogClass.java
@@ -6,7 +6,7 @@ public class LogClass {
     private static final Logger logger = LogManager.getLogger(LogClass.class);
     public static void main(String[] args) {
         logger.info("INFO Yaml");
-        logger.warn("WARN Yaml");
-        logger.error("ERROR Yaml");
+//        logger.warn("WARN Yaml");
+//        logger.error("ERROR Yaml");
     }
 }
